37d0e7bfe3aeed399d5eee88644d2536ef1529ed
takeuchi <eng.takeuchi.syun@gmail.com>
2025-01-23 03:24:34 +0900

    パスワード入力追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,35 @@
+import { useState } from 'react';
 import './App.css'
 import { Test } from './Test'
 import { Test2 } from './Test2'
 
 function App() {
+  const [password, setPassword] = useState('');
+  const passPhrase = import.meta.env.VITE_PASSWORD;
 
   return (
     <>
       <h1>リアルタイム音声文字起こし</h1>
-      <Test></Test>
-      <Test2></Test2>
+      {password !== passPhrase ? (
+        // パスワード入力フォーム
+        <div className="password-form">
+          <input
+            type="password"
+            placeholder="パスワードを入力してください"
+            aria-label="パスワード入力"
+            onChange={(e) => {
+              setPassword(e.target.value)
+            }}
+            value={password}
+          />
+        </div>
+      ) : (
+        // 認証後のコンポーネント
+        <>
+          <Test />
+          <Test2 />
+        </>
+      )}
     </>
   )
 }
