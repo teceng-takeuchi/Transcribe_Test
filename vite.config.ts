70642c7bd5062c79208a465cf91a42d389c2b1b8
takeuchi <eng.takeuchi.syun@gmail.com>
2025-01-23 05:10:35 +0900

    デプロイ関連修正

--- vite.config.ts
+++ vite.config.ts
@@ -3,4 +3,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  define: {
+    __VITE_PASSWORD__: JSON.stringify(process.env.VITE_PASSWORD),
+    __VITE_AWS_ACCESSKEY_ID__: JSON.stringify(process.env.VITE_AWS_ACCESSKEY_ID),
+    __VITE_AWS_SECRET_ACCESSKEY__: JSON.stringify(process.env.VITE_AWS_SECRET_ACCESSKEY)
+  }
 })
